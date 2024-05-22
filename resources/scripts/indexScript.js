dd054adcb8ad7cee56589e4ec4d0d2dfc658d794
LuanVictorGit <luanvictorchagas2015@gmail.com>
2024-05-22 12:04:01 -0300

    updated

--- resources/scripts/indexScript.js
+++ resources/scripts/indexScript.js
@@ -1,4 +1,5 @@
 window.onload = enable;
+var yFrom = 0, yTo = 0;
 function enable() {
     window.addEventListener("scroll", function(){
         var headerDefault = document.getElementsByClassName("header-container__nav")[0];
@@ -6,6 +7,28 @@ function enable() {
             headerDefault.classList.add("header-container__nav-scrolled");
         } else {
             headerDefault.classList.remove("header-container__nav-scrolled");
+        }
+    });
+    window.addEventListener("scroll", function () {
+        var item = document.getElementsByClassName("testScrollText")[0];
+        item.textContent = window.pageYOffset;
+    });
+    window.addEventListener("click", function (e) {
+        switch (e.target.className) {
+            case "__btn-home":
+                e.preventDefault();
+                yFrom = window.pageYOffset;
+                yTo = 0;
+                moveScroll();
+                break;
+            case "servicesButton":
+                e.preventDefault();
+                yFrom = window.pageYOffset;
+                yTo = 1030;
+                moveScroll();
+                break;
+            default:
+                break;
         }
     });
     updateMainText();
@@ -34,4 +57,26 @@ function updateMainText() {
             updateMainText();
         }, 8000);
     }
+}
+
+function moveScroll() {
+    var speed = 30;
+    if (yFrom == yTo) return;
+    if ((yFrom - yTo >= 0 && yFrom - yTo <= speed)) {
+        if (yFrom > yTo) {
+            yFrom -= 1;
+        } else {
+            yFrom += 1;
+        }
+    } else {
+        if (yFrom > yTo) {
+            yFrom -= speed;
+        } else {
+            yFrom += speed;
+        }
+    }
+    window.scrollTo(0, yFrom);
+    setTimeout(() => {
+        moveScroll();
+    }, 1);
 }
