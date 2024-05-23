cfd984dea264c28d86750f9c3c40218a03480fde
LuanVictorGit <luanvictorchagas2015@gmail.com>
2024-05-23 16:43:55 -0300

    updated

--- resources/scripts/indexScript.js
+++ resources/scripts/indexScript.js
@@ -1,34 +1,12 @@
 window.onload = enable;
 var yFrom = 0, yTo = 0;
 function enable() {
-    window.addEventListener("scroll", function(){
+    window.addEventListener("scroll", function () {
         var headerDefault = document.getElementsByClassName("header-container__nav")[0];
-        if (window.scrollY > 0){
+        if (window.scrollY > 0) {
             headerDefault.classList.add("header-container__nav-scrolled");
         } else {
             headerDefault.classList.remove("header-container__nav-scrolled");
-        }
-    });
-    window.addEventListener("scroll", function () {
-        var item = document.getElementsByClassName("testScrollText")[0];
-        item.textContent = window.pageYOffset;
-    });
-    window.addEventListener("click", function (e) {
-        switch (e.target.className) {
-            case "__btn-home":
-                e.preventDefault();
-                yFrom = window.pageYOffset;
-                yTo = 0;
-                moveScroll();
-                break;
-            case "servicesButton":
-                e.preventDefault();
-                yFrom = window.pageYOffset;
-                yTo = 1020;
-                moveScroll();
-                break;
-            default:
-                break;
         }
     });
     updateMainText();
@@ -42,7 +20,7 @@ function updateMainText() {
         textDefault = textMainBackground.textContent;
         textMainBackground.textContent = "";
     }
-    if (i < textDefault.length){
+    if (i < textDefault.length) {
         textMainBackground.textContent += textDefault[i];
         i++;
         setTimeout(() => {
@@ -57,26 +35,4 @@ function updateMainText() {
             updateMainText();
         }, 8000);
     }
-}
-
-function moveScroll() {
-    var speed = 30;
-    if (yFrom == yTo) return;
-    if ((yFrom - yTo >= 0 && yFrom - yTo <= speed)) {
-        if (yFrom > yTo) {
-            yFrom -= 1;
-        } else {
-            yFrom += 1;
-        }
-    } else {
-        if (yFrom > yTo) {
-            yFrom -= speed;
-        } else {
-            yFrom += speed;
-        }
-    }
-    window.scrollTo(0, yFrom);
-    setTimeout(() => {
-        moveScroll();
-    }, 1);
 }
